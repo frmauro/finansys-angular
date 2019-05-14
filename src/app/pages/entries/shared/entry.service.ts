79e963c9c896842b45e2753a03c889035ab006d0
francisco <frmauro8@hotmail.com>
2019-05-14 20:58:39 -0300

    improvement reports

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -4,7 +4,9 @@ import { CategoryService } from "../../categories/shared/category.service";
 import { Entry } from "./entry.model";
 
 import { observable, throwError, Observable } from "rxjs";
-import { flatMap, catchError } from "rxjs/operators";
+import { flatMap, catchError, map } from "rxjs/operators";
+
+import * as moment from "moment"; 
 
 
 @Injectable({
@@ -26,6 +28,12 @@ update(entry: Entry): Observable<Entry>{
   return this.setCategoryAndSendToServer(entry, super.update.bind(this));
 }
 
+getByMonthAndYear(month: number, year: number): Observable<Entry[]>{
+  return this.getAll().pipe(
+    map(entries => this.filterByMonthAndYear(entries, month, year))
+  )
+}
+
 private setCategoryAndSendToServer(entry: Entry, sendfn: any): Observable<Entry>{
   return this.categoryService.getById(entry.categoryId).pipe(
     flatMap(category => {
@@ -34,6 +42,16 @@ private setCategoryAndSendToServer(entry: Entry, sendfn: any): Observable<Entry>
     }),
     catchError(this.handleError)
   )
+}
+
+private filterByMonthAndYear(entries: Entry[], month: number, year: number){
+  return entries.filter(entry => {
+    const entryDate = moment(entry.date, "DD/MM/YYYY")
+    const monthMatches = entryDate.month() + 1 == month;
+    const yearMatches = entryDate.year() == year;
+
+    if (monthMatches && yearMatches) return entry;
+  })
 }
 
 
